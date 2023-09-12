660384ecb63ba697d5b0d929d7eea68493f8745f
SaharattPengsuwan <s6506022420061@email.kmutnb.ac.th>
2023-09-12 10:30:02 +0700

    ืnew

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -1,83 +1,67 @@
-
-
-
-
-
-
-const express = require("express");
-const sqlite3 = require("sqlite3");
+const express = require('express');
+const sqlite3 = require('sqlite3');
 const app = express();
 
-
-const db = new sqlite3.Database("./Database/Book.sqlite");
-
+const db = new sqlite3.Database('./Database/book.sqlite');
 
 app.use(express.json());
-
 
 db.run(`CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY,
     title TEXT,
     author TEXT
 )`);
-
-
-app.get("/books", (req, res) => {
-    db.all("SELECT * FROM books", (err, rows) => {
+app.get('/books', (req,res) => {
+    db.all('SELECT * FROM books', (err, rows)=> {
         if (err) {
             res.status(500).send(err);
-        } else {
+        }else{
             res.json(rows);
         }
     });
 });
 
-
-app.get("/books/:id", (req, res) => {
-    db.get("SELECT * FROM books WHERE id = ?", req.params.id, (err, row) => {
+app.get('/books/:id', (req,res) => {
+    db.get('SELECT * FROM books WHERE id = ?', req.params.id, (err,row) => {
         if (err) {
             res.status(500).send(err);
-        } else {
+        }else{
             if (!row) {
-                res.status(404).send("Book not found");
-            } else {
+                res.status(404).send('book not found');
+            }else{
                 res.json(row);
             }
         }
     });
 });
-
-
-app.post("/books", (req, res) => {
+app.post('/books', (req,res) =>{
     const book = req.body;
-    db.run("INSERT INTO books (title, author) VALUES (?, ?)", book.title, book.author, function(err) {
+    db.run('INSERT INTO books (title, author) VALUES (?, ?)', book.title, book.author, function(err){
         if (err) {
             res.status(500).send(err);
-        } else {
+        }else {
             book.id = this.lastID;
             res.send(book);
         }
     });
 });
 
-
-app.put('/books/:id', (req, res) => {
+app.put('/books/:id', (req,res) => {
     const book = req.body;
-    db.run("UPDATE books SET title = ?, author = ? WHERE id = ?", book.title, book.author, req.params.id, function(err) {
+    db.run('UPDATE books SET title = ?, author = ? WHERE id = ?', book.title, book.author, req.params.id, function(err){
         if (err) {
             res.status(500).send(err);
-        } else {
+        }else{
             res.send(book);
         }
     });
 });
 
-
-app.delete("/books/:id", (req, res) => {
-    db.run('DELETE FROM books WHERE id = ?'. req.params.id, function(err) {
+app.delete('/books/:id', (req,res) =>{
+    db.run('DELETE FROM books WHERE id =?', req.params.id, function(err) {
         if (err) {
             res.status(500).send(err);
-        } else {
+        }else{
             res.send({});
         }
     });
