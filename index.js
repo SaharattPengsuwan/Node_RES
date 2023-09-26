f3207075c3f76da59c3f83bf70acc4210073cffe
SaharattPengsuwan <s6506022420061@email.kmutnb.ac.th>
2023-09-26 10:15:39 +0700

    new

--- index.js
+++ index.js
@@ -1,71 +1,105 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
+const Sequelize = require('sequelize');
 const app = express();
-
-const db = new sqlite3.Database('./Database/book.sqlite');
 
 app.use(express.json());
 
-db.run(`CREATE TABLE IF NOT EXISTS books (
-    id INTEGER PRIMARY KEY,
-    title TEXT,
-    author TEXT
-)`);
-app.get('/books', (req,res) => {
-    db.all('SELECT * FROM books', (err, rows)=> {
-        if (err) {
-            res.status(500).send(err);
-        }else{
-            res.json(rows);
-        }
+
+const dbUrl = 'postgres://webadmin:QRYmts12232@node51698-saharatt1.proen.app.ruk-com.cloud:11537/book'
+
+
+
+const sequelize = new Sequelize(dbUrl)
+
+
+const Book = sequelize.define('book',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    author: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+
+sequelize.sync();
+
+
+app.get('/books', (req, res) => {
+    Book.findAll().then(books => {
+        res.json(books);
+    }).catch(err => {
+        res.status(500).send(err);
     });
 });
 
-app.get('/books/:id', (req,res) => {
-    db.get('SELECT * FROM books WHERE id = ?', req.params.id, (err,row) => {
-        if (err) {
-            res.status(500).send(err);
-        }else{
-            if (!row) {
-                res.status(404).send('book not found');
-            }else{
-                res.json(row);
-            }
+
+app.get('/books/:id', (req, res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('Book not found');
+        } else {
+            res.json(book);
         }
-    });
-});
-app.post('/books', (req,res) =>{
-    const book = req.body;
-    db.run('INSERT INTO books (title, author) VALUES (?, ?)', book.title, book.author, function(err){
-        if (err) {
-            res.status(500).send(err);
-        }else {
-            book.id = this.lastID;
-            res.send(book);
-        }
+    }).catch(err => {
+        res.status(500).send(err);
     });
 });
 
-app.put('/books/:id', (req,res) => {
-    const book = req.body;
-    db.run('UPDATE books SET title = ?, author = ? WHERE id = ?', book.title, book.author, req.params.id, function(err){
-        if (err) {
-            res.status(500).send(err);
-        }else{
-            res.send(book);
-        }
+
+app.post('/books', (req, res) => {
+    Book.create(req.body).then(book => {
+        res.send(book);
+    }).catch(err => {
+        res.status(500).send(err);
     });
 });
 
-app.delete('/books/:id', (req,res) =>{
-    db.run('DELETE FROM books WHERE id =?', req.params.id, function(err) {
-        if (err) {
-            res.status(500).send(err);
-        }else{
-            res.send({});
+
+app.put('/books/:id', (req, res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(400).send("Book not found");
+        } else {
+            book.update(req.body).then(() => {
+                res.send(book);
+            }).catch(err => {
+                res.status(500).send(err);
+            });
         }
+    }).catch(err => {
+        res.status(500).send(err);
     });
 });
+
+
+app.delete('/books/:id', (req, res) => {
+    Book.findByPk(req.params.id).then(book => {
+        if (!book) {
+            res.status(404).send('Book not found');
+        } else {
+            book.destroy().then(() => {
+                res.send({});
+            }).catch(err => {
+                res.status(500).send(err);
+                });
+        }
+}).catch(err => {
+  res.status(500).send(err);
+});
+});
+
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`))
+
+
+
+
